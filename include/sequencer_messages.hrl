8f3d6a0b986aebabafbc432558fe4047905fdaf4
ergl <ergl@users.noreply.github.com>
2021-05-18 17:02:27 +0200

    Add sequencer messages

---
+++ include/sequencer_messages.hrl
@@ -0,0 +1,39 @@
+-define(SEQUENCER_VERSION, 1).
+-define(SEQUENCER_VERSION_BITS, 8).
+-define(SEQUENCER_MSG(Payload),
+   <<?SEQUENCER_VERSION:?SEQUENCER_VERSION_BITS, Payload/binary>>
+).
+
+-record(ping, {
+   partitions :: [non_neg_integer()]
+}).
+
+-record(redblue_prepare_request, {
+    tx_id :: term(),
+    tx_label :: term(),
+    readset :: #{non_neg_integer() := [term()]},
+    writeset :: #{non_neg_integer() := #{}},
+    snapshot_vc :: #{}
+}).
+
+-record(redblue_prepare_response, {
+    tx_id :: term(),
+    %% abort reason, or commit vector
+    outcome :: atom() | #{}
+}).
+
+-record(redblue_deliver, {
+    timestamp :: non_neg_integer(),
+    %% writeset and commit vector
+    %% the writeset contains the partitions where this should be routed
+    transactions :: [{#{non_neg_integer() := #{}}, #{}}]
+}).
+
+-type sequencer_message() :: #ping{}
+                           | #redblue_prepare_request{}
+                           | #redblue_prepare_response{}
+                           | #redblue_deliver{}.
+
+-export_type([
+    sequencer_message/0
+]).
---
+++ src/sequencer_messages.erl
@@ -0,0 +1,67 @@
+-module(sequencer_messages).
+-include("sequencer_messages.hrl").
+
+-define(header(Packet, Size), (Size):(Packet)/unit:8-integer-big-unsigned).
+-define(ping, 0).
+-define(pepare_request, 1).
+-define(prepare_response, 2).
+-define(deliver, 3).
+
+-export([frame/2]).
+
+-export([ping/1,
+         prepare_request/5,
+         prepare_response/2,
+         deliver/2]).
+
+-export([decode/1]).
+
+frame(HeaderSize, Data) ->
+    Size = erlang:iolist_size(Data),
+    [<<?header(HeaderSize, Size)>>, Data].
+
+-spec ping([non_neg_integer()]) -> binary().
+ping(Partitons) ->
+    Payload = term_to_binary(Partitons),
+    <<?SEQUENCER_VERSION:?SEQUENCER_VERSION_BITS, ?ping:8, Payload/binary>>.
+
+-spec prepare_request(term(), term(), #{non_neg_integer() := [term()]}, #{non_neg_integer() := #{}}, #{}) -> binary().
+prepare_request(TxId, Label, RS, WS, VC) ->
+    Payload = term_to_binary({TxId, Label, RS, WS, VC}),
+    <<?SEQUENCER_VERSION:?SEQUENCER_VERSION_BITS, ?pepare_request:8, Payload/binary>>.
+
+-spec prepare_response(term(), atom() | #{}) -> binary().
+prepare_response(TxId, Outcome) ->
+    Payload = term_to_binary({TxId, Outcome}),
+    <<?SEQUENCER_VERSION:?SEQUENCER_VERSION_BITS, ?prepare_response:8, Payload/binary>>.
+
+-spec deliver(non_neg_integer(), [{#{non_neg_integer() := #{}}, #{}}]) -> binary().
+deliver(Ts, Transactions) ->
+    Payload = <<Ts:8/unit:8-integer-big-unsigned, (term_to_binary(Transactions))/binary>>,
+    <<?SEQUENCER_VERSION:?SEQUENCER_VERSION_BITS, ?deliver:8, Payload/binary>>.
+
+decode(<<?ping:8, Payload/binary>>) ->
+    #ping{partitions=binary_to_term(Payload)};
+
+decode(<<?pepare_request:8, Payload/binary>>) ->
+    {TxId, Label, RS, WS, VC} = binary_to_term(Payload),
+    #redblue_prepare_request{
+        tx_id=TxId,
+        tx_label=Label,
+        readset=RS,
+        writeset=WS,
+        snapshot_vc=VC
+    };
+
+decode(<<?prepare_response:8, Payload/binary>>) ->
+    {TxId, Outcome} = binary_to_term(Payload),
+    #redblue_prepare_response{
+        tx_id=TxId,
+        outcome=Outcome
+    };
+
+decode(<<?deliver:8, Ts:8/unit:8-integer-big-unsigned, Payload/binary>>) ->
+    #redblue_deliver{
+        timestamp=Ts,
+        transactions=binary_to_term(Payload)
+    }.
